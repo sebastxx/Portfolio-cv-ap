df23b07784f802aad19f676f005e790d0f706c2a
sebastxx <91297447+sebastxx@users.noreply.github.com>
2022-07-28 01:34:18 -0300

    Componentes añadidos
    header y sobre-mi

--- portfolio_cv/src/app/app.module.ts
+++ portfolio_cv/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { APLogoComponent } from './components/ap-logo/ap-logo.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    APLogoComponent,
+    SocialComponent,
+    BannerComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
