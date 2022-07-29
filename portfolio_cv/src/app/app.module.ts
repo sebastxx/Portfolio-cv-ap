6a49d2e0f02e18d38d09880a13680897b38e107a
sebastxx <91297447+sebastxx@users.noreply.github.com>
2022-07-29 01:28:27 -0300

    Componentes añadidos
    Hard&Soft Skills, Proyectos y Footer.
    Convertir header a bootstrap

--- portfolio_cv/src/app/app.module.ts
+++ portfolio_cv/src/app/app.module.ts
@@ -5,23 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { APLogoComponent } from './components/ap-logo/ap-logo.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     APLogoComponent,
-    SocialComponent,
     BannerComponent,
     SobreMiComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    SkillsComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
