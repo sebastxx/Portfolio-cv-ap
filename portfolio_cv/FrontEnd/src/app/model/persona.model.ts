6446375f7b79f664695e5234ad3ac35edb254b46
sebastxx <91297447+sebastxx@users.noreply.github.com>
2022-11-15 13:57:44 -0300

    Ultimo commit
    Último commit realizado

--- portfolio_cv/FrontEnd/src/app/model/persona.model.ts
+++ portfolio_cv/FrontEnd/src/app/model/persona.model.ts
@@ -1,10 +1,10 @@
 export class persona{
-    id?:number;
+    id?: number;
     nombre: String;
     apellido: String;
     img: String;
 
-    constructor(nombre: String, apellido: String, img: String){
+    constructor(nombre: String,apellido: String, img: String){
         this.nombre = nombre;
         this.apellido = apellido;
         this.img = img;
